aa7dacbb51fb92978f6f2dd2eacd40f087961384
Jinkyu Yi <jincreator@jincreator.net>
2011-06-19 13:08:21 +0000

    Add NFC icon(I draw it, so license free!) Finish "about" in setting menu.(also change string about_title->about, about->about_text

--- SNSMeet/gen/org/snsmeet/R.java
+++ SNSMeet/gen/org/snsmeet/R.java
@@ -12,17 +12,19 @@ public final class R {
     }
     public static final class drawable {
         public static final int icon=0x7f020000;
-        public static final int qrcode_logo_100px=0x7f020001;
-        public static final int twitter_newbird_blue_100px=0x7f020002;
+        public static final int nfc_logo_100px=0x7f020001;
+        public static final int qrcode_logo_100px=0x7f020002;
+        public static final int twitter_newbird_blue_100px=0x7f020003;
     }
     public static final class id {
-        public static final int account=0x7f070004;
-        public static final int list=0x7f070005;
+        public static final int about=0x7f070000;
+        public static final int account=0x7f070005;
+        public static final int list=0x7f070006;
         public static final int listView1=0x7f070001;
-        public static final int nfc_connect=0x7f070002;
-        public static final int qrcode_connect=0x7f070003;
-        public static final int setting=0x7f070006;
-        public static final int textView1=0x7f070000;
+        public static final int nfc_connect=0x7f070003;
+        public static final int qrcode_connect=0x7f070004;
+        public static final int setting=0x7f070007;
+        public static final int textView1=0x7f070002;
     }
     public static final class layout {
         public static final int about=0x7f030000;
@@ -38,8 +40,8 @@ public final class R {
         public static final int menu=0x7f060000;
     }
     public static final class string {
-        public static final int about=0x7f050003;
-        public static final int about_title=0x7f050004;
+        public static final int about=0x7f050004;
+        public static final int about_text=0x7f050003;
         public static final int account=0x7f050005;
         public static final int app_name=0x7f050000;
         public static final int info=0x7f050008;
