434f391306f4b7173757ac7d3af1004ac55f6174
Jinkyu Yi <jincreator@jincreator.net>
2011-06-19 15:45:31 +0000

    Make accout menu using scrollview layout. Also add facebook&twitter logo.

--- SNSMeet/gen/org/snsmeet/R.java
+++ SNSMeet/gen/org/snsmeet/R.java
@@ -11,20 +11,24 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int icon=0x7f020000;
-        public static final int nfc_logo_100px=0x7f020001;
-        public static final int qrcode_logo_100px=0x7f020002;
-        public static final int twitter_newbird_blue_100px=0x7f020003;
+        public static final int f_logo_100px=0x7f020000;
+        public static final int icon=0x7f020001;
+        public static final int nfc_logo_100px=0x7f020002;
+        public static final int qrcode_logo_100px=0x7f020003;
+        public static final int twitter_newbird_blue_100px=0x7f020004;
     }
     public static final class id {
         public static final int about=0x7f070000;
-        public static final int account=0x7f070005;
-        public static final int list=0x7f070006;
-        public static final int listView1=0x7f070001;
-        public static final int nfc_connect=0x7f070003;
-        public static final int qrcode_connect=0x7f070004;
-        public static final int setting=0x7f070007;
+        public static final int account=0x7f070008;
+        public static final int list=0x7f070009;
+        public static final int listView1=0x7f070005;
+        public static final int nfc_connect=0x7f070006;
+        public static final int qrcode_connect=0x7f070007;
+        public static final int setting=0x7f07000a;
         public static final int textView1=0x7f070002;
+        public static final int textView2=0x7f070004;
+        public static final int toggleButton1=0x7f070001;
+        public static final int toggleButton2=0x7f070003;
     }
     public static final class layout {
         public static final int about=0x7f030000;
