bd0a135e9dda5a3cce9ac6cb8ddc5f6bcf71f2b7
Jinkyu Yi <jincreator@jincreator.net>
2011-06-27 18:35:38 +0000

    Almost finish account list. Now can on/off & delete.

--- SNSMeet/src/org/snsmeet/main/Account.java
+++ SNSMeet/src/org/snsmeet/main/Account.java
@@ -3,69 +3,117 @@ package org.snsmeet.main;
 import org.snsmeet.R;
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
+import android.widget.ResourceCursorAdapter;
 import android.widget.TextView;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.Button;
+import android.widget.ToggleButton;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.LayoutInflater;
+import android.view.ViewGroup;
 import android.content.Intent;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.Cursor;
-import android.widget.SimpleCursorAdapter;
-import android.widget.BaseAdapter;
+import android.widget.CursorAdapter;
 import android.widget.TabHost;
+import android.widget.Toast;
 import android.content.Context;
+import org.snsmeet.main.AccountDB;
 
 public class Account extends Activity implements OnClickListener{
     /** Called when the activity is first created. */
 	private AccountDB accountdb;
 	private SQLiteDatabase db;
+	private Cursor cursor_twitter;
+	private Cursor cursor_facebook;
 	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         accountdb=new AccountDB(this);
         db=accountdb.getWritableDatabase();
-        Cursor cursor_twitter=db.rawQuery("SELECT * FROM twitter",null);
+        cursor_twitter=db.rawQuery("SELECT * FROM twitter",null);
         startManagingCursor(cursor_twitter);
+        cursor_facebook=db.rawQuery("SELECT * FROM facebook",null);
+        startManagingCursor(cursor_facebook);
         setContentView(R.layout.account);
         TabHost tabhost=(TabHost)findViewById(R.id.tabhost);
         tabhost.setup();
         TabHost.TabSpec spec;
-        spec=tabhost.newTabSpec("Twitter1");
-        spec.setIndicator("Twitter",getResources().getDrawable(R.drawable.twitter_newbird_blue_tab));
+        spec=tabhost.newTabSpec("twitter_tab");
+        spec.setIndicator(getString(R.string.twitter),getResources().getDrawable(R.drawable.twitter_newbird_blue_tab));
         spec.setContent(R.id.twitter_frame);
         tabhost.addTab(spec);
-        spec=tabhost.newTabSpec("Facebook2");
-        spec.setIndicator("Facebook",getResources().getDrawable(R.drawable.f_logo_tab));
+        spec=tabhost.newTabSpec("facebook_tab");
+        spec.setIndicator(getString(R.string.facebook),getResources().getDrawable(R.drawable.f_logo_tab));
         spec.setContent(R.id.facebook_frame);
         tabhost.addTab(spec);
-        SimpleCursorAdapter adapter_twitter=new SimpleCursorAdapter(this,R.layout.account_list,cursor_twitter,new String[]{"nick"},new int[]{R.id.account_nick});
         ListView twitter_list=(ListView)findViewById(R.id.twitter_listview);
-        twitter_list.setAdapter(adapter_twitter);
-        Cursor cursor_facebook=db.rawQuery("SELECT * FROM facebook",null);
-        startManagingCursor(cursor_facebook);
-        SimpleCursorAdapter adapter_facebook=new SimpleCursorAdapter(this,R.layout.account_list,cursor_facebook,new String[]{"nick"},new int[]{R.id.account_nick});
+        AccountAdapter twitterAdapter=new AccountAdapter(this,cursor_twitter,"twitter");
+        twitter_list.setAdapter(twitterAdapter);
+        AccountAdapter facebookAdapter=new AccountAdapter(this,cursor_facebook,"facebook");
         ListView facebook_list=(ListView)findViewById(R.id.facebook_listview);
-        facebook_list.setAdapter(adapter_facebook);
+        facebook_list.setAdapter(facebookAdapter);
         tabhost.setCurrentTab(0);
         View account_add_twitter=findViewById(R.id.account_add_twitter);
         account_add_twitter.setOnClickListener(this);
         View account_add_facebook=findViewById(R.id.account_add_facebook);
         account_add_facebook.setOnClickListener(this);
     }
-	
+	class AccountAdapter extends CursorAdapter{
+		private String service;
+		private Cursor cursor;
+		public AccountAdapter(Context context, Cursor cur, String service_input) {
+            super(context,cur);
+            service=service_input;
+            cursor=cur;
+        }
+
+        @Override
+        public View newView(Context context, Cursor cur, ViewGroup parent) {
+            LayoutInflater li = LayoutInflater.from(context);
+            return li.inflate(R.layout.account_list, parent, false);
+        }
+
+        @Override
+        public void bindView(View view, Context context, Cursor cur) {
+        	final int cs=cur.getInt(cur.getColumnIndex("_id"));
+        	TextView nick = (TextView)view.findViewById(R.id.account_nick);
+        	final ToggleButton use = (ToggleButton)view.findViewById(R.id.account_toggle);
+        	nick.setText(cur.getString(cur.getColumnIndex("nick")));
+        	use.setChecked(cur.getLong(cur.getColumnIndex("use"))==1);
+        	use.setOnClickListener(new OnClickListener(){
+        		public void onClick(View v){
+        			db.execSQL("UPDATE "+service+" SET use="+(int)(use.isChecked()?1:0)+" WHERE _id="+cs+";");
+        		}
+        	});
+            ImageView delete = (ImageView)view.findViewById(R.id.account_delete);
+            delete.setOnClickListener(new OnClickListener() {
+                
+                public void onClick(View v) {
+    				db.execSQL("DELETE FROM "+service+" WHERE _id='"+cs+"';");
+    				cursor.requery();
+                }
+            });
+        }
+    }
+		
 	public void onClick(View v){
 		switch(v.getId()){
 		case R.id.account_add_twitter:
 			final Intent twitter=new Intent(this,Twitter_Add.class);
 			startActivity(twitter);
-			db.execSQL("INSERT INTO twitter VALUES (null,'abcd','efgh','higk');");
-				break;
+			accountdb.insert_twitter(db,"abcd","efgh","higk");
+			cursor_twitter.requery();
+			break;
 		case R.id.account_add_facebook:
-			db.execSQL("INSERT INTO facebook VALUES (null,'dcba','efgh','higk');");
+			accountdb.insert_facebook(db,"dcba","efgh","higk");
+			cursor_facebook.requery();
 			break;
 		}
 	}
--- SNSMeet/src/org/snsmeet/main/AccountDB.java
+++ SNSMeet/src/org/snsmeet/main/AccountDB.java
@@ -11,8 +11,8 @@ public class AccountDB extends SQLiteOpenHelper {
     }
     @Override
     public void onCreate(SQLiteDatabase db){
-    	db.execSQL("CREATE TABLE twitter(_id  INTEGER PRIMARY KEY AUTOINCREMENT, nick TEXT, token TEXT, use INTEGER);");
-    	db.execSQL("CREATE TABLE facebook(_id  INTEGER PRIMARY KEY AUTOINCREMENT, nick TEXT, token TEXT, use INTEGER);");
+    	db.execSQL("CREATE TABLE twitter(_id INTEGER PRIMARY KEY AUTOINCREMENT, nick TEXT, token TEXT, token_secret TEXT, use INTEGER);");
+    	db.execSQL("CREATE TABLE facebook(_id INTEGER PRIMARY KEY AUTOINCREMENT, nick TEXT, token TEXT, token_secret TEXT, use INTEGER);");
     }
     @Override
     public void onUpgrade(SQLiteDatabase db,int oldVersion,int newVersion){
@@ -21,9 +21,21 @@ public class AccountDB extends SQLiteOpenHelper {
     	onCreate(db);
     }
     public void insert_twitter(SQLiteDatabase db,String nick, String token, String token_secret){
-    	db.execSQL("INSERT INTO twitter VALUES (null,'"+nick+"','"+token+"','"+token_secret+"','"+"1');");
+    	db.execSQL("INSERT INTO twitter VALUES (null,'"+nick+"','"+token+"','"+token_secret+"','1');");
+    }
+    public void delete_twitter(SQLiteDatabase db,int id){
+    	db.execSQL("DELETE FROM twitter WHERE _id='"+id+"';");
+    }
+    public void use_twitter(SQLiteDatabase db, boolean onoff, int id){
+    	db.execSQL("UPDATE twitter SET use="+(int)(onoff?1:0)+" WHERE _id="+id+";");
     }
     public void insert_facebook(SQLiteDatabase db,String nick, String token, String token_secret){
-    	db.execSQL("INSERT INTO facebook VALUES (null,'"+nick+"','"+token+"','"+token_secret+"','"+"1');");
+    	db.execSQL("INSERT INTO facebook VALUES (null,'"+nick+"','"+token+"','"+token_secret+"','1');");
+    }
+    public void delete_facebook(SQLiteDatabase db,int id){
+    	db.execSQL("DELETE FROM facebook WHERE _id='"+id+"';");
+    }
+    public void use_facebook(SQLiteDatabase db, boolean onoff, int id){
+    	db.execSQL("UPDATE facebook SET use="+(int)(onoff?1:0)+" WHERE _id="+id+";");
     }
 }
